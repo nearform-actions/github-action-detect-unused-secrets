a876dbec70ef18806d182578cd37f49d56c14f6b
Semra Shukrieva <semrashukrieva@SemraS-SPMAC22.local>
2022-02-25 11:11:21 +0100

    feat: initial implementation missing extracted secrets from actions

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Detect unused secrets GitHub Action
+
 Detect unused github secrets
+
+# github-action-detect-unused-secrets
+
+GitHub Action that automatically detects unused secrets comparing the workflow secret names with the repo secret names.
+
+## Example usage
+
+```yaml
+name: detect-unused-secrets
+
+on: [push]
+
+jobs:
+  setup:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Detect unused secrets
+        uses: nearform/github-action-detect-unused-secrets@v1
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Usage
+
+Configure this action in your workflows providing the inputs described below.
+
+### `github-token`
+
+**Required** A GitHub token. For additional information read this guide --> [#about-the-github_token-secret](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret)
+
+This should be a token with access to your repository scoped in as a secret.
+The YML workflow will need to set github-token with the GitHub Secret Token
+github-token: ${{ secrets.GITHUB_TOKEN }}
--- src/action.js
+++ src/action.js
@@ -1,4 +1,41 @@
-// import * as core from '@actions/core'
-// import * as github from '@actions/github'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-export async function run() {}
+export async function run() {
+  core.info(`
+  *** ACTION RUN - START ***
+  `)
+  // get all the repo secrets
+  // check what secrets are used in the workflows
+  // log error for unused secrets
+
+  const githubToken = core.getInput('github-token', { required: true })
+  const octokit = github.getOctokit(githubToken)
+  const { owner, repo } = github.context.repo
+
+  try {
+    const { secrets } = await octokit.rest.actions.listRepoSecrets({
+      owner,
+      repo
+    })
+
+    const unusedSecrets = []
+    secrets.forEach(secret => {
+      if (github.context.workflow.includes(secret.name)) {
+        unusedSecrets.push(secret.name)
+      }
+    })
+
+    if (unusedSecrets.length) {
+      core.setFailed(`
+      Unused secrets detected: ${unusedSecrets.join(', ')}
+      `)
+    }
+  } catch (err) {
+    core.setFailed(`Action failed with error ${err}`)
+  } finally {
+    core.info(`
+    *** ACTION RUN - END ***
+    `)
+  }
+}
